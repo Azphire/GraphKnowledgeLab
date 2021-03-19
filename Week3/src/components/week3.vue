22697ee10b9e3fdf0dac4ccc7491ac9f765f9fb2
Zihan <azphire@163.com>
2021-03-20 00:41:10 +0800

    引入了echarts

--- Week3/src/components/week3.vue
+++ Week3/src/components/week3.vue
@@ -1,12 +1,13 @@
 <template>
     <div class="week3">
-        this is week3 homework
         <el-container>
-            <el-aside>
-                边栏
+            <el-header>顶栏</el-header>
+            <el-aside width="200px">
+                <div>边栏</div>
             </el-aside>
             <el-main>
                 树状图
+                <div id="echart" style="width:400px;height:400px;"></div>
             </el-main>
         </el-container>
     </div>
@@ -14,10 +15,36 @@
 
 <script>
 export default {
-  name: 'Week3',
-  data () {
+    name: 'Week3',
+    data () {
     return {
     }
-  }
+    },
+    mounted (){
+		this.echartsInit()
+	},
+    methods: {
+    echartsInit () {
+		this.$echarts.init(document.getElementById('echart')).setOption({
+            xAxis: {
+                type: 'category',
+                data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+            },
+            yAxis: {
+                type: 'value'
+            },
+            series: [{
+                data: [120, 200, 150, 80, 70, 110, 130],
+                type: 'bar',
+                showBackground: true,
+                backgroundStyle: {
+                    color: 'rgba(220, 220, 220, 0.8)'
+                }
+            }]
+			})
+		}
+
+    }
+
 }
 </script>
