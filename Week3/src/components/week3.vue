e054680a1a3085c1540fa8061e56f61eca2e3279
1854039 <45162720+1854039@users.noreply.github.com>
2021-03-22 15:07:40 +0800

    翻页ver1
    页面数据少的时候有问题

--- Week3/src/components/week3.vue
+++ Week3/src/components/week3.vue
@@ -1,38 +1,50 @@
 <template>
   <div class="week3">
     <el-container>
-      <el-aside width="250px">
+      <el-aside width="300px">
         <div>
           <el-pagination
+            style="width: 100%"
             @size-change="handleSizeChange"
             @current-change="handleCurrentChange"
-            :page-sizes="[4, 5, 6, 7, 8]"
+            :pager-count="2"
+            :page-sizes="[4, 5, 6, 14, 15]"
             :current-page="currentPage"
             :page-size="pagesize"
-            :total="4"
+            :total="totalNum"
             layout="total, sizes, prev, pager, next, jumper"
+            
           >
           </el-pagination>
         </div>
         <div>
-          <el-table style="width: 100%" stripe :data="leftlist">
-            <el-table-column>
-              <el-col>
-                <el-row>
-                  <div>
-                    {{ leftlist[0].path }}
-                  </div>
-                </el-row>
-                <el-row>
-                  <div>
-                    <el-tag effect="plain">
-                      {{ leftlist[0].duration }}
-                    </el-tag>
-                    <span>{{ leftlist[0].startpoint }} </span>
-                  </div>
-                </el-row>
-              </el-col>
-            </el-table-column>
+          <el-table
+            
+            stripe
+            :data="tableData.slice((currentPage-1)*pagesize,currentPage*pagesize)"
+            highlight-current-row
+            :row-class-name="rowClassName"
+            @row-click="whileClick"
+    
+          >
+
+          
+              <el-table-column prop="path">
+                  <template slot-scope="scope">
+                    <el-row>
+                      <div>{{scope.row.path}}</div>
+                    </el-row>
+                    <el-row>
+                      <div>
+                              <el-tag effect="plain">
+                                {{ scope.row.duration }}
+                              </el-tag>
+                              <span>{{ scope.row.startpoint }} </span>
+                            </div>
+                    </el-row>
+                  </template>
+              </el-table-column>
+      
           </el-table>
         </div>
       </el-aside>
@@ -46,7 +58,7 @@
               justify="space-between"
               align="middle"
             >
-              <div class="title">{{ leftlist[0].path }}</div>
+              <div class="title">{{ leftlist[rownumber].path }}</div>
               <el-button type="primary" size="small">查看日志</el-button>
             </el-row>
 
@@ -57,7 +69,7 @@
               align="middle"
             >
               <el-tag effect="plain" size="mini">
-                {{ leftlist[0].name }}
+                {{ leftlist[rownumber].name }}
               </el-tag>
             </el-row>
 
@@ -69,13 +81,13 @@
             >
               <el-col>
                 <el-tag effect="dark" type="info" size="mini"> 起始点 </el-tag>
-                <span>{{ leftlist[0].startpoint }} </span>
+                <span>{{ leftlist[rownumber].startpoint }} </span>
                 <el-tag effect="dark" type="info" size="mini">
                   持续时间
                 </el-tag>
-                <span>{{ leftlist[0].duration }} </span>
+                <span>{{ leftlist[rownumber].duration }} </span>
                 <el-tag effect="dark" type="info" size="mini"> 跨度 </el-tag>
-                <span>{{ leftlist[0].span }} </span>
+                <span>{{ leftlist[rownumber].span }} </span>
               </el-col>
               <el-col
                 ><el-button type="mini">列表</el-button>
@@ -96,10 +108,14 @@
 </template>
 
 <script>
+import tableData from '../data/data.json'
 export default {
   name: "Week3",
   data() {
     return {
+      tableData:tableData.leftlist,
+      totalNum:21,
+      rownumber: 0,
       currentPage: 1,
       pagesize: 2,
       tree: {
@@ -225,77 +241,149 @@ export default {
       leftlist: [
         {
           path: "/projectC/{value}",
-          name: "c64945c8-49c4-490c-a05d-9418e4d0a792",
-          startpoint: "2021-03-19 22:17:59",
-          duration: "64737 ms",
+          name: "a64945c8-49c4-490c-a05d-9418e4d0a792",
+          startpoint: "2021-03-11 22:17:59",
+          duration: "64730 ms",
           span: "20",
         },
         {
           path: "/projectC/{value}",
-          name: "c64945c8-49c4-490c-a05d-9418e4d0a792",
-          startpoint: "2021-03-19 22:17:59",
-          duration: "64737 ms",
-          span: "20",
+          name: "b64945c8-49c4-490c-a05d-9418e4d0a792",
+          startpoint: "2021-03-12 22:17:59",
+          duration: "64731 ms",
+          span: "21",
         },
         {
           path: "/projectC/{value}",
           name: "c64945c8-49c4-490c-a05d-9418e4d0a792",
-          startpoint: "2021-03-19 22:17:59",
+          startpoint: "2021-03-13 22:17:59",
+          duration: "64732 ms",
+          span: "22",
+        },
+        {
+          path: "/projectC/{value}",
+          name: "d64945c8-49c4-490c-a05d-9418e4d0a792",
+          startpoint: "2021-03-14 22:17:59",
+          duration: "64733 ms",
+          span: "23",
+        },
+        {
+          path: "/projectC/{value}",
+          name: "e64945c8-49c4-490c-a05d-9418e4d0a792",
+          startpoint: "2021-03-15 22:17:59",
+          duration: "64734 ms",
+          span: "24",
+        },
+        {
+          path: "/projectC/{value}",
+          name: "f64945c8-49c4-490c-a05d-9418e4d0a792",
+          startpoint: "2021-03-16 22:17:59",
+          duration: "64735 ms",
+          span: "25",
+        },
+        {
+          path: "/projectC/{value}",
+          name: "g64945c8-49c4-490c-a05d-9418e4d0a792",
+          startpoint: "2021-03-17 22:17:59",
+          duration: "64736 ms",
+          span: "26",
+        },
+        {
+          path: "/projectC/{value}",
+          name: "h64945c8-49c4-490c-a05d-9418e4d0a792",
+          startpoint: "2021-03-18 22:17:59",
           duration: "64737 ms",
+          span: "27",
+        },
+        {
+          path: "/projectC/{value}",
+          name: "i64945c8-49c4-490c-a05d-9418e4d0a792",
+          startpoint: "2021-03-19 22:17:59",
+          duration: "64738 ms",
+          span: "28",
+        },
+        {
+          path: "/projectC/{value}",
+          name: "j64945c8-49c4-490c-a05d-9418e4d0a792",
+          startpoint: "2021-03-19 22:17:59",
+          duration: "64739 ms",
+          span: "29",
+        },
+        {
+          path: "/projectC/{value}",
+          name: "a64945c8-49c4-490c-a05d-9418e4d0a792",
+          startpoint: "2021-03-11 22:17:59",
+          duration: "64730 ms",
           span: "20",
         },
         {
           path: "/projectC/{value}",
-          name: "c64945c8-49c4-490c-a05d-9418e4d0a792",
-          startpoint: "2021-03-19 22:17:59",
-          duration: "64737 ms",
-          span: "20",
+          name: "b64945c8-49c4-490c-a05d-9418e4d0a792",
+          startpoint: "2021-03-12 22:17:59",
+          duration: "64731 ms",
+          span: "21",
         },
         {
           path: "/projectC/{value}",
           name: "c64945c8-49c4-490c-a05d-9418e4d0a792",
-          startpoint: "2021-03-19 22:17:59",
-          duration: "64737 ms",
-          span: "20",
+          startpoint: "2021-03-13 22:17:59",
+          duration: "64732 ms",
+          span: "22",
         },
         {
           path: "/projectC/{value}",
-          name: "c64945c8-49c4-490c-a05d-9418e4d0a792",
-          startpoint: "2021-03-19 22:17:59",
-          duration: "64737 ms",
-          span: "20",
+          name: "d64945c8-49c4-490c-a05d-9418e4d0a792",
+          startpoint: "2021-03-14 22:17:59",
+          duration: "64733 ms",
+          span: "23",
         },
         {
           path: "/projectC/{value}",
-          name: "c64945c8-49c4-490c-a05d-9418e4d0a792",
-          startpoint: "2021-03-19 22:17:59",
-          duration: "64737 ms",
-          span: "20",
+          name: "e64945c8-49c4-490c-a05d-9418e4d0a792",
+          startpoint: "2021-03-15 22:17:59",
+          duration: "64734 ms",
+          span: "24",
         },
         {
           path: "/projectC/{value}",
-          name: "c64945c8-49c4-490c-a05d-9418e4d0a792",
-          startpoint: "2021-03-19 22:17:59",
-          duration: "64737 ms",
-          span: "20",
+          name: "f64945c8-49c4-490c-a05d-9418e4d0a792",
+          startpoint: "2021-03-16 22:17:59",
+          duration: "64735 ms",
+          span: "25",
         },
         {
           path: "/projectC/{value}",
-          name: "c64945c8-49c4-490c-a05d-9418e4d0a792",
-          startpoint: "2021-03-19 22:17:59",
-          duration: "64737 ms",
-          span: "20",
+          name: "g64945c8-49c4-490c-a05d-9418e4d0a792",
+          startpoint: "2021-03-17 22:17:59",
+          duration: "64736 ms",
+          span: "26",
         },
         {
           path: "/projectC/{value}",
-          name: "c64945c8-49c4-490c-a05d-9418e4d0a792",
-          startpoint: "2021-03-19 22:17:59",
+          name: "h64945c8-49c4-490c-a05d-9418e4d0a792",
+          startpoint: "2021-03-18 22:17:59",
           duration: "64737 ms",
-          span: "20",
+          span: "27",
+        },
+        {
+          path: "/projectC/{value}",
+          name: "i64945c8-49c4-490c-a05d-9418e4d0a792",
+          startpoint: "2021-03-19 22:17:59",
+          duration: "64738 ms",
+          span: "28",
+        },
+        {
+          path: "/projectC/{value}",
+          name: "j64945c8-49c4-490c-a05d-9418e4d0a792",
+          startpoint: "2021-03-19 22:17:59",
+          duration: "64739 ms",
+          span: "29",
         },
       ],
     };
   },
+
+
   mounted() {
     this.echartsInit();
   },
@@ -303,10 +391,22 @@ export default {
     handleSizeChange(val) {
       console.log(`每页 ${val} 条`);
       this.pagesize = val;
+      this.currentPage=1
     },
     handleCurrentChange(val) {
       console.log(`当前页: ${val}`);
-      this.currentPage = currentPage;
+      this.currentPage = val;
+    },
+    rowClassName({ row, rowIndex }) {
+      row.index = rowIndex;
+    },
+
+    whileClick(row) {
+      this.rownumber = row.index;
+      console.log("number");
+      console.log(row);
+      console.log(row.index);
+      console.log(this.rownumber);
     },
     echartsInit() {
       this.$echarts.init(document.getElementById("echart")).setOption({
